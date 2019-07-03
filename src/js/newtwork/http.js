48009287e0fad808c58a5f2d075d9935337f2dd1
stillfak <vadim.gavrikov.1998@gmail.com>
2019-07-03 16:25:19 +0300

    init

---
+++ src/js/newtwork/http.js
@@ -0,0 +1,70 @@
+import fetch from 'isomorphic-fetch';
+import qs from 'query-string';
+
+
+
+const
+    BASE = '/api',
+    headers = {'Content-Type': 'application/json'};
+
+const request =
+    (url) =>
+        (options={}) =>
+            ({path='', query={}}={}) =>
+                (data) => {
+                    const body = JSON.stringify(data);
+                    query = qs.stringify(query);
+                    query = query ? `?${query}` : '';
+                    path = path ? `/${path}` : '';
+                    return (
+                        fetch(
+                            `${BASE}${url}${path}${query}`,
+                            {
+                                ...options,
+                                ...(body ? { body } : {})
+                            }
+                        ).then((response) => response.json())
+                    );
+                };
+
+function get(r) {
+    return r();
+}
+
+function patch(r) {
+    return r({ headers, method: 'PATCH' });
+}
+
+function post(r) {
+    return r({ headers, method: 'POST' });
+}
+
+function update(r) {
+    return r({ headers, method: 'PUT' });
+}
+
+function remove(r) {
+    return r({ method: 'DELETE' });
+}
+
+const requests = {
+    authorise: request('/authorise'),
+    users: request('/users'),
+};
+
+export const users = {
+    create: post(requests.users)(),
+    item: (id) => get(requests.users)({ path: id })(),
+    list: (query) => get(requests.users)({ query })(),
+    patch: (id) => patch(requests.users)({ path: id }),
+    remove: (id) => remove(requests.users)({ path: id }),
+    update: (id) => update(requests.users)({ path: id }),
+};
+
+export const authorise = update(requests.authorise);
+
+export const api = {
+    authorise, users
+};
+
+export default api;
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: "body"
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+
+        })
+    ]
+    // devServer: {
+    //   overlay: true
+    // }
+};
